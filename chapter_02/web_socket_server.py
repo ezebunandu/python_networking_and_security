22b8a846250532e8ba975024614c2cef2249ae4b
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2022-02-16 23:01:32 -0700

    Async web socket client and server

---
+++ chapter_02/web_socket_server.py
@@ -0,0 +1,22 @@
+from aiohttp import web
+import socketio
+
+socket_io = socketio.AsyncServer()
+app = web.Application()
+socket_io.attach(app)
+
+
+async def index(request):
+    return web.Response(text="Hello world from socketio", content_type="text/html")
+
+
+@socket_io.on("message")
+def print_message(socket_id, data):
+    print(f"Socket ID: {socket_id}")
+    print(f"Data: {data}")
+
+
+app.router.add_get("/", index)
+
+if __name__ == "__main__":
+    web.run_app(app)
