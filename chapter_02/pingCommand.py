543a0543c5405b850c4ee0b1c019c464e7a0463b
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2022-08-13 21:31:46 -0600

    subprocess run has different interface from Popen

--- chapter_02/pingCommand.py
+++ chapter_02/pingCommand.py
@@ -2,8 +2,10 @@ import subprocess
 import sys
 import shlex
 
-p = subprocess.run(
-    shlex.split("/sbin/ping -c 1 www.google.com"), shell=False, stderr=subprocess.PIPE
+p = subprocess.Popen(
+    shlex.split("/sbin/ping -c 1 www.google.com"),
+    shell=False,
+    stderr=subprocess.PIPE,
 )
 out = p.stderr.read(1)
 sys.stdout.write(str(out.decode("utf-8")))
